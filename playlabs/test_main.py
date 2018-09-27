48a3c3d2704b1f36cb1d0f431a6346990446c91e
jpic <jpic@yourlabs.org>
2018-09-27 17:10:27 +0200

    wip

---
+++ playlabs/test_main.py
@@ -0,0 +1,8 @@
+from .main import nostrict
+
+
+def test_nostrict():
+    result = nostrict([])
+    assert len(result) == 2
+    assert result[0] == '--ssh-extra-args'
+    assert 'StrictHostKeyChecking=no' in result[1]
