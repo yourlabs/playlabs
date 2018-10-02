c9a1770d5cba5a6a7d1e7df016d2340723b0b184
jpic <jamespic@gmail.com>
2018-10-02 12:06:05 +0200

    Bugfix backup/restore
    Bug found in backup: it would always write the /restic directory on the
    FTP. This has no side effect on production, but that means that
    instances on staging server have been overriding each others backups on
    the FTP.
    
    Bug found in restore: it would not find the backups in the expected
    path.

--- playlabs/roles/backup/templates/backup.sh
+++ playlabs/roles/backup/templates/backup.sh
@@ -7,5 +7,8 @@ export RESTIC_REPOSITORY={{ repo }}
 pushd {{ project_home }}
 {{ script_content_pre }}
 restic backup $backup
+{% if project_backup_lftp_dsn|default(False) %}
+lftp -c 'set ssl:check-hostname false;connect {{ backup_lftp_dsn }}; mkdir -p {{ project_instance }}; mirror -Rv {{ project_home }}/restic {{ project_instance }}/restic'
+{% endif %}
 {{ script_content_post }}
 popd
