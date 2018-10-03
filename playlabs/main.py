c907fac2c7c1b04bca07545d9be0ff9b3d75e96e
Thomas Mignot <tmignot@student.42.fr>
2018-10-04 00:17:46 +0200

    UT

--- playlabs/main.py
+++ playlabs/main.py
@@ -214,13 +214,25 @@ class Parser(object):
         self.makeinstall = False
         self.makedeploy = False
         self.makeinit = False
-        self.user = None
+        self._user = None
         self.roles = []
         self.hosts = []
         self.options = []
         self.password = None
         self.subvars = dict()
         self.options_dict = dict()
+        self.argcount = 0
+
+    @property
+    def user(self):
+        return self._user
+
+    @user.setter
+    def user(self, name):
+        if self._user:
+            raise NameError('User should only be defined once')
+        else:
+            self._user = name
 
     def handle_install(self, arg):
         if arg:
@@ -253,13 +265,18 @@ class Parser(object):
             self.options += ['--user', self.user]
 
     def handle_inventory(self, arg):
+        if not arg:
+            raise Exception('Inventory: missing parameter')
         for i in arg.split(','):
             if os.path.exists(i):
                 self.options += ['-i', i]
             else:
+                raise NameError(f'Inventory not found: {i}')
                 print(f'command line inventory {i} cannot be found')
 
     def handle_plugins(self, arg):
+        if not arg:
+            raise Exception('Plugins: missing parameter')
         plugins_path = os.path.join(os.path.dirname(__file__), 'plugins')
         if os.path.exists(plugins_path):
             plugins_list = os.listdir(plugins_path)
@@ -268,28 +285,46 @@ class Parser(object):
                 if p in plugins_list:
                     plugins.append(p)
                 else:
-                    print(f'command line plugin {p} cannot be found')
+                    self.argcount += 1
+                    raise NameError(f'Plugin not found: {p}')
             if plugins:
                 self.options.append('-e')
                 if len(plugins) > 1:
                     self.options.append(f'plugins={",".join(plugins)}')
                 else:
                     self.options.append(f'plugins={plugins[0]}')
-            else:
-                print('no plugins specified')
+            else:  # should be useless
+                raise NameError('Plugin: "-p" found but no plugins specified')
 
     def handle_vars(self, arg):
         if arg == '-e':
             return
 
         if '=' in arg and not arg.startswith('--'):
-            if '.' in arg.split('=')[0]:
-                descriptor, value = arg.split('=')
-                variable, attribute = descriptor.split('.')
-                self.subvars[attribute] = value
+            if arg[0] == '=' or arg[-1] == '=':
+                raise NameError(f'Wrong variable format {arg}, \
+variable definition cannot start neither end with "="')
+            name = arg.split('=')[0]
+            if '.' in name:
+                if name[0] == '.' or name[-1] == '.':
+                    raise NameError(f'Wrong variable format {name}, \
+variable name cannot start neither end with "."')
+
+                def setattribute(a, v):
+                    if len(a) > 1:
+                        return {a[0]: setattribute(a[1:], v)}
+                    else:
+                        return {a[0]: v}
+                descriptor, val = arg.split('=')
+                variable = descriptor.split('.')[0]
+                descarray = descriptor.split('.')[1:]
+                if variable not in self.subvars.keys():
+                    self.subvars[variable] = setattribute(descarray, val)
+                else:
+                    self.subvars[variable].update(setattribute(descarray, val))
             else:
                 self.options += ['-e', arg]
-                self.options_dict[arg.split('=')[0]] = arg.split('=')[1]
+                self.options_dict[name] = arg.split('=')[1]
         else:
             self.options.append(arg)
 
@@ -310,9 +345,11 @@ class Parser(object):
     def parse(self, args):
         while args:
             arg = args.pop(0)
+            self.argcount += 1
 
             if arg in ('-u', '--user'):
                 self.user = args.pop(0)
+                self.argcount += 1
             elif arg.startswith('-u=') or arg.startswith('--user='):
                 self.user = arg.split('=')[-1]
             else:
@@ -320,8 +357,9 @@ class Parser(object):
                     self.handle_host(arg)
                 elif arg in ['init', 'deploy']:
                     self.handles[arg](None)
-                elif arg in self.primary_tokens and not self.options:
+                elif arg in self.primary_tokens:
                     self.handles[arg](args.pop(0) if args else None)
+                    self.argcount += 1
                 else:
                     self.handle_vars(arg)
 
@@ -336,7 +374,7 @@ class Parser(object):
         if self.subvars:
             self.options += ['-e', json.dumps(self.subvars)]
 
-        if not getattr(self, 'user', None):
+        if not self.user:
             self.user = os.getenv('USER')
 
         self.print()
@@ -458,10 +496,17 @@ def cli():  # noqa
         sys.exit(0)
     commands = ['scaffold']
     parser = Parser()
-    if sys.argv[1] in commands:
-        parser.parse(sys.argv[2:])
-    else:
-        parser.parse(sys.argv[1:])
+    try:
+        if sys.argv[1] in commands:
+            parser.argcount = 2
+            parser.parse(sys.argv[2:])
+        else:
+            parser.argcount = 1
+            parser.parse(sys.argv[1:])
+    except Exception as e:
+        sys.argv[parser.argcount - 1] += '<---'
+        print(f'\nError parsing command line input: {e}\n')
+        print(' '.join(sys.argv) + '\n\n')
 
     # todo: check if connection works with provided credentials if any
     # otherwise try without credentials, and strip them from now on
---
+++ playlabs/tests/test_parser_func.py
@@ -0,0 +1,165 @@
+"""
+##### PARAMETERS #####
+
+ test -u,--user username[:password], -u=username, --user=username
+ test default_user
+ test [username[:password]]@host
+ test localhost options
+ test ssh_config
+ test vars, subvars, ansible options
+ test roles
+ test inventories
+ test plugins
+"""
+
+import os
+import sys
+import pytest
+import json
+
+sys.path.append('../')
+
+from main import Parser
+
+
+@pytest.fixture
+def p():
+    return Parser()
+
+
+def test_parser_class_exists(p):
+    assert p
+
+
+def test_user_name_shortopt(p):
+    p.parse(['-u', 'testname'])
+    assert p.user == 'testname'
+
+
+def test_user_name_longopt(p):
+    p.parse(['--user', 'testname2'])
+    assert p.user == 'testname2'
+
+
+def test_user_name_shortvar(p):
+    p.parse(['-u=testname1'])
+    assert p.user == 'testname1'
+
+
+def test_user_name_longvar(p):
+    p.parse(['--user=testname2'])
+    assert p.user == 'testname2'
+
+
+def test_user_default(p):
+    p.parse([])
+    assert p.user == os.getenv('USER')
+
+
+def test_at_host_only(p):
+    p.parse(['@testhost'])
+    assert p.hosts == ['testhost']
+
+
+def test_user_at_host(p):
+    p.parse(['testname3@testhost2'])
+    assert p.user == 'testname3'
+    assert p.hosts == ['testhost2']
+
+
+def test_user_pass_at_host(p):
+    p.parse(['testname4:password@testhost3'])
+    assert p.user == 'testname4'
+    assert p.hosts == ['testhost3']
+    assert p.password == 'password'
+    assert '--ask-become-pass' in p.options
+    assert '--ask-pass' in p.options
+
+
+def test_localhost(p):
+    p.parse(['@localhost'])
+    assert '-c' in p.options
+    assert 'local' in p.options
+    assert p.options.index('local') == p.options.index('-c') + 1
+
+
+def test_ssh_config(p):
+    p.parse(['@host'])
+    assert '--ssh-extra-args' in p.options
+
+
+def test_vars(p):
+    p.parse([
+        '--extra=opt',
+        'extra-opt2',
+        'extra=var'
+    ])
+    assert 'extra-opt2' in p.options
+    assert '--extra=opt' in p.options
+    assert p.options[p.options.index('--extra=opt') - 1] != '-e'
+    assert 'extra=var' in p.options
+    assert p.options[p.options.index('extra=var') - 1] == '-e'
+
+
+def test_subvars(p):
+    p.parse(['env.testsub=value'])
+    assert '-e' in p.options
+    assert json.dumps({'env': {'testsub': 'value'}}) in p.options
+
+
+def test_deep_vars(p):
+    p.parse(['test.very.deep.var=deep'])
+    assert json.dumps({
+        'test': {
+            'very': {
+                'deep': {'var': 'deep'}
+            }}}) in p.options
+
+
+def test_merge_vars(p):
+    p.parse(['test.sub=value1', 'test.sub2=value2'])
+    assert json.dumps({
+        'test': {
+            'sub': 'value1',
+            'sub2': 'value2'
+            }}) in p.options
+
+
+def test_role_uniq(p):
+    p.parse(['install', 'rolename'])
+    assert 'rolename' in p.roles
+
+
+def test_role_multi(p):
+    roles = 'nginx,docker,firewall'
+    p.parse(['install', roles])
+    assert p.roles == roles.split(',')
+
+
+def test_plugin_uniq(p):
+    p.parse(['-p', 'django'])
+    assert 'plugins=django' in p.options
+
+
+def test_plugin_multi(p):
+    plugins = 'django,postgres,sentry'
+    p.parse(['-p', plugins])
+    assert f'plugins={plugins}' in p.options
+    assert p.options[p.options.index(f'plugins={plugins}')-1] == '-e'
+
+
+def test_inventory_uniq(p):
+    invpath = '../inventory_template/inventory.yaml'
+    p.parse(['-i', invpath])
+    assert invpath in p.options
+    assert p.options[p.options.index(invpath) - 1] == '-i'
+
+
+def test_inventory_multi(p):
+    invpath1 = '../inventory_template/inventory.yaml'
+    invpath2 = '../init.yml'
+    p.parse(['-i', ','.join([invpath1, invpath2])])
+    assert invpath1 in p.options
+    assert p.options[p.options.index(invpath1) - 1] == '-i'
+    assert invpath2 in p.options
+    assert p.options[p.options.index(invpath2) - 1] == '-i'
