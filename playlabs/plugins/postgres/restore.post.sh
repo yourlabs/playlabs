037e62d346be1ae457f2210bbfd7577b3e2dc2ed
jpic <jamespic@gmail.com>
2018-10-04 02:44:26 +0200

    Typo

--- playlabs/plugins/postgres/restore.post.sh
+++ playlabs/plugins/postgres/restore.post.sh
@@ -5,7 +5,7 @@ docker start {{ project_postgres_host }}
 until test -S {{ project_postgres_run_home }}/.s.PGSQL.5432; do
     sleep 1
 done
-@sleep 3 # ugly wait until db starts up, socket waiting aint enough
+sleep 3 # ugly wait until db starts up, socket waiting aint enough
 docker exec {{ project_postgres_host }} psql -U {{ project_postgres_user }} -f {{ project_postgres_data_mount }}/data.dump
 echo Restore happy, clearing $postgres_current and $restore/data.dump
 rm -rf $postgres_current $restore/data.dump
