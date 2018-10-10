acb6179d7ffea39dc18541cd5e1dca7a52076b15
jpic <jpic@yourlabs.org>
2018-10-10 16:53:25 +0200

    Bugfix dynamic image name

--- playlabs/roles/project/templates/docker-run.sh
+++ playlabs/roles/project/templates/docker-run.sh
@@ -11,7 +11,7 @@ docker run -d --name {{ project_instance }} --restart unless-stopped \
     {% for key, value in project_env.items() %}
     -e {{ key }}='{% if value.replace is defined %}{{ value.replace("'", "\\'") }}{% else %}{{ value }}{% endif %}' \
     {% endfor %}
-    {{ project_image }}
+    $(<{{ project_home }}/docker-image)
 docker network connect {{ project_instance }} {{ project_instance }}
 {% for network in project_networks -%}
 docker network connect {{ network }} {{ project_instance }}
