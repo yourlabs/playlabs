987b932adfe6b414a583324f4281074cc09a7921
tmig <tmig@yourlabs.org>
2018-11-22 21:05:39 +0100

    dependencies

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
             'playlabs = playlabs.cli.main:_cli',
         ],
     },
+    install_requires=['clitoo', 'processcontroller'],
     classifiers=[
         'Development Status :: 1 - Planning',
         'Environment :: Web Environment',
