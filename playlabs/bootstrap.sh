f0824bff0a028537c2d4dd6fb5a3791b640a8f28
jpic <jpic@yourlabs.org>
2019-01-30 17:21:08 +0100

    Add bootstrap by default / remove init step

---
+++ playlabs/bootstrap.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -x
+if [ -f /etc/playlabs/bootstrap_done ]; then
+    exit 0
+fi
+
+sudo=""
+[ "$USER" = "root" ] || sudo=sudo
+
+if which python3; then
+    if ! which python; then
+        $sudo ln -sfn $(which python3) /usr/bin/python
+    fi
+    exit 0
+fi
+
+if which apt; then
+    $sudo apt update -y
+    $sudo apt install -y python3
+elif which pacman; then
+    $sudo pacman -Sy --noconfirm
+    $sudo pacman -S --noconfirm python
+elif which apk; then
+    $sudo apk update
+    $sudo apk add python
+fi
+
+date > /etc/playlabs/bootstrap_done
